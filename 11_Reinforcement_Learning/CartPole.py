b43a7e0ebdc8a05bedd0ec8ae98853e4a99a14b8
ofialko <oleksandrfialko@gmail.com>
2018-06-18 16:07:41 +1200

    staging

--- 11_Reinforcement_Learning/CartPole.py
+++ 11_Reinforcement_Learning/CartPole.py
@@ -10,19 +10,26 @@ import gym
 from gym import wrappers
 import numpy as np
 
-env = gym.make('CartPole-v0')
-env.observation_space.shape[0]
-env.action_space
-
-
-
-env = gym.make('FrozenLake-v0')
-env.observation_space
-env.action_space.sample()
-
-env = gym.make('MountainCar-v0')
-env.observation_space.high
-env.action_space.sample()
+# env = gym.make('FrozenLake-v0')
+# obs = env.reset()
+# act=env.action_space.sample()
+# x = env.step(act);x
+#
+# # env.reset()
+# env.observation_space.sample()
+# env.action_space
+#
+#
+#
+# env = gym.make('FrozenLake-v0')
+# env.reset()
+# env.observation_space
+# env.action_space.sample()
+#
+# env = gym.make('MountainCar-v0')
+# env.reset()
+# env.observation_space.high
+# env.action_space.sample()
 
 
 class RandomAgent(object):
@@ -30,6 +37,15 @@ class RandomAgent(object):
         self.env = env
         self.n_policy = n_policy
         self.obs_space = env.observation_space
+        if len(self.obs_space.shape) == 0:
+            self.type = "Discrete"
+            self.obs = np.zeros(self.obs_space.n)
+        elif len(self.obs_space.shape) == 1:
+            self.type = "Box"
+            self.obs = np.zeros(self.obs_space.shape)
+        else:
+            raise Exception('Observatio space is not supported')
+
         self.act_space = env.action_space
         self.best_policy = None
 
@@ -50,7 +66,13 @@ class RandomAgent(object):
         else:
             raise ValueError
 
-
+    def gen_obs(self,obs):
+        if self.type == 'Discrete':
+            x = self.obs.copy()
+            x[obs] = 1
+            return x
+        else:
+            return obs
     # def gen_random_policy(self):
     #     return (self.observation_space.sample(), np.random.uniform(-1,1))
 
@@ -69,7 +91,9 @@ class RandomAgent(object):
         obs = self.env.reset()
         total_reward = 0
         for i in range(t_max):
-            selected_action = self.policy_to_action(policy, obs)
+
+            obs_1 = self.gen_obs(obs)
+            selected_action = self.policy_to_action(policy, obs_1)
             obs, reward, done, _ = self.env.step(selected_action)
             total_reward += reward
             if done:
@@ -78,7 +102,7 @@ class RandomAgent(object):
         return total_reward
 
     def train(self):
-        policy_list = self.gen_policy_list(self.n_policy)
+        policy_list = self.gen_policy_list()
         # Evaluate the score of each policy.
         scores_list = [self.run_episode(policy=p) for p in policy_list]
         # Select the best plicy.
@@ -87,9 +111,9 @@ class RandomAgent(object):
 
 
 if __name__ == '__main__':
-    env = gym.make('CartPole-v0')
+    env = gym.make('MountainCar-v0')
     outdir = '11_Reinforcement_Learning/CartPole-results'
-    agent = RandomAgent(env)
+    agent = RandomAgent(env,500)
     agent.train()
     print('Training Done')
     reward = agent.run_episode(outdir = outdir)
