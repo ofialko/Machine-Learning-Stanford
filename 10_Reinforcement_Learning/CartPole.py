1cba1d33e7143cf31c28ae2d5b480912c908abba
ofialko <oleksandrfialko@gmail.com>
2018-06-27 11:12:26 +1200

    Adding Evolution Agent

--- 10_Reinforcement_Learning/CartPole.py
+++ 10_Reinforcement_Learning/CartPole.py
@@ -1,16 +1,10 @@
-'''
-Classical problem in RL: Solving the CartPole environment.
-It simulates an inverted pendulum mounted upwards on cart,
-the pendulum is intially vertical and the goal is to maintain it vertically balanced.
-The only way to control the pendulum is by choosing a horizontal direction for the cart to move
-either to left or right. Random Search solution is provided.
-'''
 
-from Utilities import RandomAgent, gym
+from Utilities import *
 if __name__ == '__main__':
-    env = gym.make('Pendulum-v0')
-    outdir = '11_Reinforcement_Learning/CartPole-results'
-    agent = RandomAgent(env,500)
+    env = gym.make('Acrobot-v1')
+    outdir = 'Results'
+    #agent = RandomAgent(env,500)
+    agent = EvolutionAgent(env,100,10)
     agent.train()
     print('Training Done')
     reward = agent.run_episode(outdir = outdir)
--- 10_Reinforcement_Learning/Utilities.py
+++ 10_Reinforcement_Learning/Utilities.py
@@ -3,6 +3,8 @@ from gym import wrappers
 import numpy as np
 
 class RandomAgent(object):
+    '''Generates a set of n_policy random solutions
+       and selects the one with the highest reward'''
     def __init__(self,env,n_policy):
         self.env = env
         self.n_policy = n_policy
@@ -52,8 +54,7 @@ class RandomAgent(object):
             return x
         else:
             return obs
-    # def gen_random_policy(self):
-    #     return (self.observation_space.sample(), np.random.uniform(-1,1))
+
 
     def gen_policy_list(self):
         ## Generate a pool or random policies
@@ -88,5 +89,52 @@ class RandomAgent(object):
         # Evaluate the score of each policy.
         scores_list = [self.run_episode(policy=p) for p in policy_list]
         # Select the best plicy.
-        print('Best policy score = %f' %max(scores_list))
+        print('Best policy score = {0:f}'.format(max(scores_list)))
         self.best_policy = policy_list[np.argmax(scores_list)]
+
+
+class EvolutionAgent(RandomAgent):
+    def __init__(self,env,n_policy,n_steps):
+        super().__init__(env,n_policy)
+        self.n_steps = n_steps
+
+    def crossover(self,policy1, policy2):
+        new_policy = policy1.copy()
+        for i in range(self.x):
+            for j in range(self.y):
+                rand = np.random.uniform()
+                if rand > 0.5:
+                    new_policy[i,j] = policy2[i,j]
+        return new_policy
+
+    def mutation(self,policy, p=0.05):
+        new_policy = policy.copy()
+        for i in range(self.x):
+            for j in range(self.y):
+                rand = np.random.uniform()
+                if rand < p:
+                    new_policy[i,j] = np.random.randn()
+        return new_policy
+
+
+    def train(self):
+        policy_list = self.gen_policy_list()
+        for idx in range(self.n_steps):
+            # Evaluate the score of each policy
+            policy_scores = [self.run_episode(policy=p) for p in policy_list]
+            print('Generation {0:d} : max score = {1:0.2f}'.format(idx+1, max(policy_scores)))
+            policy_ranks = list(reversed(np.argsort(policy_scores)))
+            elite_set = [policy_list[x] for x in policy_ranks[:5]]
+            select_probs = np.array(policy_scores) / np.sum(policy_scores)
+            child_set = [self.crossover(
+                policy_list[np.random.choice(range(self.n_policy), p=select_probs)],
+                policy_list[np.random.choice(range(self.n_policy), p=select_probs)])
+                for _ in range(self.n_policy - 5)]
+
+            mutated_list = [self.mutation(p) for p in child_set]
+            policy_list = elite_set
+            policy_list += mutated_list
+
+        policy_scores = [self.run_episode(policy=p) for p in policy_list]
+        print('Best policy score = {0:f}'.format(max(policy_scores)))
+        self.best_policy = policy_list[np.argmax(policy_scores)]
